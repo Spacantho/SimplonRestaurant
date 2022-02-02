32214eb8c81c88a4899bf2a24a1f0dad2391ce6f
Spacantho <anthony.tourolle08@gmail.com>
2022-02-02 12:46:24 +0100

    Navbar

---
+++ assets/includes/navbar.php
@@ -0,0 +1,20 @@
+<header>
+    <div id="hamburger">
+        <img id="logo" src="assets/images/LogoSimplonResto.png" alt="LogoSimplonResto">
+            <div id="hamburger-content">
+                <nav>
+                    <ul id="nav-list-choice">
+                        <li class="nav-choice" id="nav-accueil"><a href="#"><i class="fas fa-home icon-nav"></i>Accueil</a></li>
+                        <li class="nav-choice" id="nav-menu"><a href="#"><i class="fas fa-hamburger icon-nav"></i>Menu</a></li>
+                        <li class="nav-choice" id="nav-contact"><a href="#"><i class="fas fa-envelope icon-nav"></i>Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <button id="hamburger-button">&#9776</button>
+            <div id="hamburger-sidebar">
+                <div id="hamburger-sidebar-header"><img id="logo-sidebar" src="assets/images/LogoSimplonResto.png" alt="LogoSimplonResto"></div>
+                <div id="hamburger-sidebar-body"></div>
+            </div>
+        <div id="hamburger-overlay"></div>
+    </div>
+</header>
