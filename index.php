bfa948a72275de79fcabf0fe33bd8df4af16fc68
TeddyH08 <teddy.hemonet@gmail.com>
2022-02-02 15:58:45 +0100

    acceuil

--- index.php
+++ index.php
@@ -12,9 +12,54 @@
 <body>
     <?php include("assets/includes/navbar.php") ?>
     <script src="assets/js/main.js"></script>
+
     <div class="banniere">
         <img src="assets/images/LogoSimplonResto.png" class="logo">
     </div>
+
+    <div class="desc">
+        <div class="boxd">
+            <div class="contenud">
+                <h2 class="titreh">Description du restaurant :</h2>
+                <p class="texth">A Charleville-Mézières, Simplon Restaurant vous propose des produits de type FastFood dans un cadre agréable et chaleureux, à deux pas de tout commerces et autres.</p>
+                <p class="texth">Simplon Restaurant, créé en 1960 par Mr.PETIT Romain, est un restaurant alliant rapidité des FastFood et qualités des plus plus grands restaurants étoilés.</p>
+                <p class="texth"><span>Tous le monde est le bienvenue chez Simplon Restaurant !!</span></p>
+            </div>
+        </div>
+
+        <div class="separator"></div>
+
+    </div>
+
+    <div class="autre">
+        <div class="horaire">
+            <div class="heure">
+                <h2 class="titreh">Horaires :</h2>
+                <p class="texth">Lundi : 10h30-14h30 / 17h30-00h00</p>
+                <p class="texth">Mardi : 10h30-14h30 / 17h30-00h00</p>
+                <p class="texth">Mercredi : 10h30-14h30 / 17h30-00h00</p>
+                <p class="texth">Jeudi : 10h30-14h30 / 17h30-00h00</p>
+                <p class="texth">Vendredi : 10h30-14h30 / 17h30-02h00</p>
+                <p class="texth">Samedi : 10h30-15h / 17h-02h00</p>
+                <p class="texth">Dimanche : Fermé !</p>
+            </div>
+        </div>
+
+        <div class="separatorh"></div>
+
+        <div class="platdj">
+            <div class="repasdj">
+                <h2 class="titreh">Plat du jour :</h2>
+                <h3 class="titres">Journée spéciale St-Valentin !!</h3>
+                <p class="texth">Burger des amoureux</p>
+                <p class="texth">Accompagnement : frites</p>
+                <p class="texth">Fondant au chocolat</p>
+                <p class="texth">Boisson au choix</p>
+            </div>
+        </div>
+    </div>
+
+    <?php include("assets/includes/footer.php") ?>
 
 </body>
 </html>
