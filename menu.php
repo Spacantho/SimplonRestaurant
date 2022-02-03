ef0a183cfbd340d45310d90ad95f4e699ad50b71
Spacantho <anthony.tourolle08@gmail.com>
2022-02-03 20:52:01 +0100

    Menu complet sans le js de malade qui fait des pop up

--- menu.php
+++ menu.php
@@ -9,7 +9,207 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <?php include('assets/includes/navbar.php'); ?>
+
+<?php include('assets/includes/navbar.php'); ?>
+    <div id="menu-corps">
+        <img id="menu-left-side-img" src="assets/images/Menu-left-side-burger.jpg" alt="bannière verticale burger">
+            <div id="menu">
+                <div id="menu-titre"><p>Simplon Restaurant <br/> <span id="menu-sous-titre">Depuis 2015</span></p></div>
+                    <div id="menu-separation">
+                        <div id="menu-left">
+                            <div id="menu-horizontal-separator"></div>
+                                <div class="menu-categorie"><p>Pizza :</p></div>
+                                    <div class="menu-liste-plats">
+                                        <div class="menu-plats">
+                                            <p> 
+                                                La Margarita</br>
+                                                La Cinq Fromages</br>
+                                                La Chorizo</br>
+                                                La Cannibale</br>
+                                                La Végétarienne</br> 
+                                                La Chicken Cesar</br>
+                                            </p>
+                                        </div>
+                                        <div class="menu-prix">
+                                            <p> 
+                                                6€</br>
+                                                7.50€</br>
+                                                6.50€</br>
+                                                8€</br>
+                                                6.50€</br>
+                                                8.50€</br>
+                                            </p>
+                                        </div>
+                                    </div>
+                            <div id="menu-horizontal-separator"></div>
+                                <div class="menu-categorie"><p>Burgers<span id=frites> + frites </span>:</p></div>
+                                    <div class="menu-liste-plats">
+                                        <div class="menu-plats">
+                                            <p> 
+                                                Hamburger</br>
+                                                Cheese</br>
+                                                Bacon</br>
+                                                Chicken</br>
+                                                Veggie</br>
+                                            </p>
+                                        </div>
+                                        <div class="menu-prix">
+                                            <p> 
+                                                4€</br>
+                                                4.50€</br>
+                                                4.50€</br>
+                                                4.50€</br>
+                                                4€</br>
+                                            </p>
+                                        </div>
+                                    </div>
+                            <div id="menu-horizontal-separator"></div>
+                                <div class="menu-categorie"><p>Pâtes :</p></div>
+                                    <div class="menu-liste-plats">
+                                        <div class="menu-plats">
+                                            <p> 
+                                                Carbonara</br>
+                                                Bolognaise</br>
+                                            </p>
+                                        </div>
+                                        <div class="menu-prix">
+                                            <p> 
+                                                3€</br>
+                                                3.50€</br>
+                                            </p>
+                                        </div>
+                                    </div>  
+                            <div id="menu-horizontal-separator"></div>
+                                <div class="menu-categorie"><p>Desserts :</p></div>
+                                    <div class="menu-liste-plats">
+                                        <div class="menu-plats">
+                                            <p> 
+                                                Fondant au chocolat</br>
+                                                Îles flottantes</br>
+                                                Poire Belle-Hélène</br>
+                                            </p>
+                                        </div>
+                                        <div class="menu-prix">
+                                            <p> 
+                                                2€</br>
+                                                2€</br>
+                                                2€</br>
+                                            </p>
+                                        </div>
+                                    </div>  
+                            <div id="menu-horizontal-separator"></div>
+                                <div class="menu-categorie"><p>Plat du jour :</p></div>
+                                    <div class="menu-liste-plats">
+                                        <div class="menu-plats">
+                                            <p id="plat-spe"> 
+                                                Burger des amoureux</br><span id="menu-event-plat-spe">Spécial Saint-Valentin</span>
+                                            </p>
+                                        </div>
+                                        <div class="menu-prix">
+                                            <p> 
+                                                4€</br>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                        <div id="menu-separator"></div>
+                            <div id="menu-right">
+                                <div id="menu-horizontal-separator"></div>
+                                    <div class="menu-categorie"><p>Softs :</p></div>
+                                        <div class="menu-liste-plats">
+                                            <div class="menu-plats">
+                                                <p> 
+                                                    Coca Cola 33cl</br>
+                                                    Lipton IceTea 33cl</br>
+                                                    Sprite 33cl</br>
+                                                    Oasis 33cl</br>
+                                                    Redbull 25cl</br>
+                                                    Monster 50cl</br>
+                                                </p>
+                                            </div>
+                                            <div class="menu-prix">
+                                                <p> 
+                                                    1€</br>
+                                                    1€</br>
+                                                    1€</br>
+                                                    1€</br>
+                                                    0.20€</br>
+                                                    4€</br>
+                                                </p>
+                                            </div>
+                                        </div>
+                                <div id="menu-horizontal-separator"></div>
+                                    <div class="menu-categorie"><p>Alcools :</p></div>
+                                        <div class="menu-liste-plats">
+                                            <div class="menu-plats">
+                                                <p> 
+                                                    8.6 50cl</br>
+                                                    Triple Karmeliet 33cl</br>
+                                                    Leffe 33cl</br>
+                                                    Vin Rouge 75cl</br>
+                                                    Whisky 4cl</br>
+                                                    Get 27 5cl</br>
+                                                    Jagermeister 50cl</br>
+                                                </p>
+                                            </div>
+                                            <div class="menu-prix">
+                                                <p> 
+                                                    0.50€</br>
+                                                    1.50€</br>
+                                                    1€</br>
+                                                    8€</br>
+                                                    1.20€</br>
+                                                    1.20€</br>
+                                                    5€</br>
+                                                </p>
+                                            </div>
+                                        </div>
+                                <div id="menu-horizontal-separator"></div>
+                                    <div class="menu-categorie"><p>Healthy :</p></div>
+                                        <div class="menu-liste-plats">
+                                            <div class="menu-plats">
+                                                <p> 
+                                                    Salade César</br>
+                                                    Galettes de Brocoli</br>
+                                                    Croquettes de semoule</br>
+                                                    Tarte aux courgettes</br>
+                                                </p>
+                                            </div>
+                                            <div class="menu-prix">
+                                                <p> 
+                                                    4€</br>
+                                                    3€</br>
+                                                    3€</br>
+                                                    4€</br>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div id="menu-horizontal-separator"></div>
+                                <div class="menu-categorie"><p>Plâts spéciaux :</p></div>
+                                    <div class="menu-liste-plats">
+                                        <div class="menu-plats">
+                                            <p>
+                                                Couscous</br>
+                                                Chilli Con Carne</br>
+                                                Fajitas</br>
+                                            </p>
+                                        </div>
+                                        <div class="menu-prix">
+                                            <p> 
+                                                4€</br>
+                                                4€</br>
+                                                4€</br>
+                                            </p>
+                                        </div>
+                                    </div>
+                                
+
+                        </div>
+                    </div>
+                </div>    
+            </div>  
+
     <script src="assets/js/main.js"></script>
 </body>
 </html>
