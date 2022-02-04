d5e266291d9bb946059db57b36c64ba76e21d813
TeddyH08 <teddy.hemonet@gmail.com>
2022-02-04 14:26:45 +0100

    slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,9 +1,17 @@
 var counter = 1;
 
+window.onload = function(){
+    changerimage();
+}
+
 setInterval(function() {
+    changerimage()
+}, 5000);
+
+function changerimage(){
     document.getElementById('radio' + counter).checked = true;
     counter++;
-    if(counter > 4) {
+    if (counter > 4) {
         counter = 1;
     }
-}, 5000);
+}
