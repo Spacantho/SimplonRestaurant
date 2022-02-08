3ae3ae8c5fd64772c0a72b9ac75b6463473ae9bd
Teddy1402 <98819362+Teddy1402@users.noreply.github.com>
2022-02-08 09:05:42 +0100

    Create backtotop.php

---
+++ assets/includes/backtotop.php
@@ -0,0 +1,7 @@
+<div id="back-to-top">
+      
+      <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fas fa-arrow-up"></i></button>
+      
+</div>
+
+<script src="assets/js/backtotopjs.js"></script>
